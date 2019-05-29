5e494cff2f65e7b94f2d10f94e9e513bdaf47377
Adam <admkearney@gmail.com>
2019-05-28 20:55:53 -0400

    basic styling and index page

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
+  <div class="font-sans text-grey-800 leading-normal flex flex-col min-h-screen">
+    <header>
+      <nav class="container mx-auto flex justify-between items-center py-8">
+        <p></p>
+        <ul class="uppercase tracking-wide flex items-center">
+          <li class="mr-8"><a href="#" class="text-gray-600 hover:text-green-600">Work</a></li>
+          <li class="mr-8"><a href="#" class="text-gray-600 hover:text-green-600">Contact</a></li>
+        </ul>
       </nav>
-    </header>
-    <slot/>
+    </header> 
+    <div class="flex-grow">
+      <slot/>
+    </div>
+    <footer>
+      <div class="container mx-auto flex items-center justify-between py-8">
+        <ul class="flex items-center mb-8">
+          <li class="mr-8">
+            <a href="https://github.com/adamkea" class="text-gray-600 hover:text-green-600">Github</a>
+          </li>
+          <li class="mr-8">
+            <a href="https://www.linkedin.com/in/admkearney/" class="text-gray-600 hover:text-green-600">LinkedIn</a>
+          </li>
+          <li class="mr-8">
+            <a href="mailto:adamkearney@live.com" class="text-gray-600 hover:text-green-600">Email</a>
+          </li>
+        </ul>
+      </div>  
+    </footer>
   </div>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,19 @@
 <template>
   <Layout>
-    
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p class="text-white bg-blue-500">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="hero container-inner mx-auto flex justify-between py-16">
+      <div class="text-3xl text-gray-800 font-bold tracking-wide">
+        Hello! I'm Adam, a <a href="https://github.com/adamkea">software developer</a>
+        living in Toronto, currenlty working for 
+        <a href="https://www.openapp.ie/">Openapp</a>.
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Adam Kearney | Software Developer'
   }
 }
 </script>
